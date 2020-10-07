fbab9542c7e6f4fb118b06ff6a93f0885da2edcb
Sheng Zhan <jameszhan02@hotmail.com>
2020-10-07 14:53:19 -0400

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+-- add initial employee test data
+INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mr.','Bigshot','Smartypants','(555)555-5551','bs@abc.com');
+INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mrs.','Penny','Pincher','(555)555-5551','pp@abc.com');
+INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mr.','Smoke','Andmirrors','(555)555-5552','sa@abc.com');
+INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mr.','Sam','Slick','(555)555-5552','ng@abc.com');
+INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mr.','Sloppy','Joe','(555)555-5553','sj@abc.com');
+ INSERT INTO Employee (Title,FirstName,LastName,PhoneNo,Email)
+ VALUES ('Mr.','Sheng','Zhan','(555)555-5553','what@the.com');
